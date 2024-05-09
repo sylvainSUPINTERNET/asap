b795cd450f2bf181d4f1945eb3c3630c6a66aa18
sylvainSUPINTERNET <sylvain.joly00@gmail.com>
2024-05-09 18:07:41 +0200

    [ALL POSSIBLE VALUES]

--- src/main/java/com/go/asap/AsapApplication.java
+++ src/main/java/com/go/asap/AsapApplication.java
@@ -5,10 +5,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.ArrayList;
-import java.util.BitSet;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 @SpringBootApplication
 public class AsapApplication implements CommandLineRunner {
@@ -30,6 +27,8 @@ public class AsapApplication implements CommandLineRunner {
 		characteristicValueIndex.put("BB_03", 3);
 		characteristicValueIndex.put("CC_01", 4);
 		characteristicValueIndex.put("CC_02", 5);
+		characteristicValueIndex.put("XX_01", 6);
+
 
 		List<Row> pseudoTable = new ArrayList<>();
 
@@ -51,6 +50,7 @@ public class AsapApplication implements CommandLineRunner {
 		table1.add(row1);
 
 		Row row2 = new Row();
+		row2.setActive(true);
 		var b2 = new BitSet();
 		b2.set(characteristicValueIndex.get("AA_02"));
 		b2.set(characteristicValueIndex.get("BB_03"));
@@ -74,6 +74,7 @@ public class AsapApplication implements CommandLineRunner {
 		table2.add(row3);
 
 		Row row4 = new Row();
+		row4.setActive(true);
 		var b4 = new BitSet();
 		b4.set(characteristicValueIndex.get("AA_02"));
 		b4.set(characteristicValueIndex.get("BB_03"));
@@ -81,39 +82,87 @@ public class AsapApplication implements CommandLineRunner {
 		row4.setBitset(b4);
 		table2.add(row4);
 
+
+		List<Row> table3 = new ArrayList<>();
+		Row row5 = new Row();
+		row5.setActive(true);
+		var b5 = new BitSet();
+		b5.set(characteristicValueIndex.get("AA_01"));
+		b5.set(characteristicValueIndex.get("BB_02"));
+		b5.set(characteristicValueIndex.get("CC_01"));
+		b5.set(characteristicValueIndex.get("XX_01"));
+		row5.setBitset(b5);
+		table3.add(row5);
+
+		/*
+		 * AA | BB | CC | XX
+		 * ------------
+		 * 01 | 02 | 01   01
+		 */
+
 		allTables.add(table1);
 		allTables.add(table2);
+		allTables.add(table3);
 
 
-		for ( List<Row> incomingTable : allTables ) {
-			if ( pseudoTable.isEmpty() ) {
-				System.out.println("Init");
+
+		for (List<Row> incomingTable : allTables) {
+			if (pseudoTable.isEmpty()) {
+				System.out.println("Initializing pseudoTable with the first table.");
 				pseudoTable.addAll(incomingTable);
-			} else {
+				continue;
+			}
 
-				System.out.println("Merge check coherance");
-				for ( Row rowsIncomingTable : incomingTable ) {
-					System.out.println("________________________________________");
-					System.out.println("Compare incoming row : " + rowsIncomingTable.getBitset());
+			boolean hasDisabledLine;
+			do {
+				hasDisabledLine = false;
+				Set<Row> rowsToKeep = new HashSet<>();
 
-					for ( Row rowsPseudoTable : pseudoTable ) {
-						var intersection = (BitSet) rowsPseudoTable.getBitset().clone();
-						intersection.and(rowsIncomingTable.getBitset());
-
-						System.out.println(" VS existing : " + rowsPseudoTable.getBitset() + " => " + intersection);
-						if ( intersection.isEmpty() ) {
-							System.out.println("No intersection for row : " + rowsPseudoTable.getBitset() + " and " + rowsIncomingTable.getBitset());
-							System.out.println("Remove row : " + rowsPseudoTable.getBitset());
+				for (Row incomingTableRow : incomingTable) {
+					System.out.println("Incoming: " + incomingTableRow.getBitset());
+					for (Row pseudoTableRow : pseudoTable) {
+						if (!pseudoTableRow.isActive()) {
+							System.out.println("Skipped (disabled): " + pseudoTableRow.getBitset());
+							continue;
 						}
 
+						BitSet intersection = (BitSet) pseudoTableRow.getBitset().clone();
+						intersection.and(incomingTableRow.getBitset());
+
+						if (!intersection.isEmpty()) {
+							System.out.println("Valid intersection found: " + intersection);
+							// Fusionner le BitSet de pseudoTableRow avec celui de incomingTableRow
+							BitSet mergedBitSet = (BitSet) pseudoTableRow.getBitset().clone();
+							mergedBitSet.or(incomingTableRow.getBitset());  // Union des deux BitSets
+							pseudoTableRow.setBitset(mergedBitSet);  // Mettre à jour le BitSet de pseudoTableRow
+							rowsToKeep.add(pseudoTableRow);
+							System.out.println("Merged BitSet: " + mergedBitSet);
+
+						} else {
+							System.out.println("No valid intersection for: " + pseudoTableRow.getBitset());
+						}
 					}
 				}
-			}
+
+				// Disable rows that are not in rowsToKeep
+				for (Row pseudoTableRow : pseudoTable) {
+					if (pseudoTableRow.isActive() && !rowsToKeep.contains(pseudoTableRow)) {
+						pseudoTableRow.setActive(false);
+						System.out.println("Disabling row: " + pseudoTableRow.getBitset());
+						hasDisabledLine = true;
+					}
+				}
+			} while (hasDisabledLine);
 		}
 
 
-		System.out.println(pseudoTable);
 
+		System.out.println(" -- Active Rows in Pseudo Table -- ");
+		for (Row row : pseudoTable) {
+			if (row.isActive()) {  // Vérifier si la ligne est active
+				System.out.println("Active Row: " + row.getBitset());
+			}
+		}
 
 
 
